a5d531bcc3be19d9df075936a301cc8c183e8db7
Isaac Sultan <isaacsultan1@gmail.com>
2019-02-08 13:39:13 +0000

    added condition

--- test/EthTeller.test.js
+++ test/EthTeller.test.js
@@ -51,6 +51,13 @@ contract("EthTeller", function([_, adminRole, brokerRole, user]) {
         from: adminRole
       });
     });
+    it("should revert if the user does not have sufficient ETH", async function() {
+      const largeAmount = ether("1000");
+      shouldFail.reverting.withMessage(
+        this.ethTeller.deposit({ from: user, value: largeAmount }),
+        "Returned error: sender doesn't have enough funds to send tx"
+      );
+    });
     it("should increase the balance of CollateralTeller when ether is sent", async function() {
       await this.ethTeller.deposit({ from: user, value: value });
       (await balance.difference(this.ethTeller.address, async () => {
@@ -59,7 +66,10 @@ contract("EthTeller", function([_, adminRole, brokerRole, user]) {
     });
     it("should add to users ETH collateral balance", async function() {
       await this.ethTeller.deposit({ from: user, value: value });
-      (await this.managingDirector.clientCollateral(user, toBytes("ETH"))).should.be.bignumber.equal(value);
+      (await this.managingDirector.clientCollateral(
+        user,
+        toBytes("ETH")
+      )).should.be.bignumber.equal(value);
     });
   });
   describe("#withdraw", async function() {
@@ -68,22 +78,23 @@ contract("EthTeller", function([_, adminRole, brokerRole, user]) {
       await this.managingDirector.addBrokerRole(this.ethTeller.address, {
         from: adminRole
       });
-    })
+    });
     it("should revert if there not sufficient collateral", async function() {
       shouldFail.reverting(this.ethTeller.withdraw(value), { from: user });
     });
     it("should decrease the balance of CollateralTeller when ether is withdrawn", async function() {
       (await balance.difference(this.ethTeller.address, async () => {
         await this.ethTeller.deposit({ from: user, value: value });
-        await this.ethTeller.withdraw(ether("6"), {from: user});
+        await this.ethTeller.withdraw(ether("6"), { from: user });
       })).should.be.bignumber.equal(ether("4"));
     });
     it("should remove from the users ETH collateral balance", async function() {
       await this.ethTeller.deposit({ from: user, value: value });
-      await this.ethTeller.withdraw(ether("6"), {from: user});
-      (await this.managingDirector.clientCollateral(user, toBytes("ETH"))).should.be.bignumber.equal(
-        ether("4")
-      );
+      await this.ethTeller.withdraw(ether("6"), { from: user });
+      (await this.managingDirector.clientCollateral(
+        user,
+        toBytes("ETH")
+      )).should.be.bignumber.equal(ether("4"));
     });
   });
 });
